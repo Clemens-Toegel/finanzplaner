a2465d23d99b8b62d6b51d3a22c9546a2b5e730c
clemitdev <clemitdev@gmail.com>
2026-02-08 19:22:09 +0100

    chore: change bundle id

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.finanzplaner"
+    namespace = "at.clemitdev.pilo"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
